2758f85387a5544d9c87557da3d117a3a31a1008
skeown13 <code@keown.net>
2020-06-30 12:20:24 -0700

    created a tableDataArray within tableData.js and a waitingListDataArray within waitingListData.js in order to hold the reservation and waitlist data. exported them both

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+let tableDataArray = [
+  {
+    customerName: "Stacy",
+    customerPhoneNumber: 1234567,
+    customerEmail: "no@no.no",
+    customerUniqueID: "offline"
+  }
+]
+
+module.exports = tableDataArray
---
+++ data/waitingListData.js
@@ -0,0 +1,10 @@
+let waitingListDataArray = [
+  {
+    customerName: "Brian",
+    customerPhoneNumber: 2345678,
+    customerEmail: "nono0@nono.nono",
+    customerUniqueID: "online"
+  }
+]
+
+module.exports = waitingListDataArray
